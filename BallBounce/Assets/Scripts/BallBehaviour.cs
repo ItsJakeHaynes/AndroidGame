5fc9b9eaebc0c575318c249af02257725abb84ea
Jake Haynes <47613806+ItsJakeHaynes@users.noreply.github.com>
2020-09-12 00:27:39 +0100

    Start of game
    initial upload

---
+++ BallBounce/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float bounceForce;
+
+    public float maxSpeed = 350f;
+
+    bool GameStarted;
+
+    public void Awake() {
+
+       rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GameStarted) {
+
+            if(Input.anyKeyDown) {
+
+                GameStarted = true;
+                StartInitalBounce();
+                GameManager.instance.GameStart();
+            }
+        }
+    }
+
+    void StartInitalBounce() {
+
+        //Vector2 randomDirection = new Vector2(Random.Range(-1,  1),   1);
+        //rb.AddForce(randomDirection * 10, ForceMode2D.Impulse);
+
+        rb = GetComponent<Rigidbody2D> ();
+        rb.AddForce(Vector2.up * maxSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if(collision.gameObject.tag == "CheckFall") {
+            
+            GameManager.instance.Restart();
+
+        }
+
+        else if (collision.gameObject.tag =="Paddle") {
+
+            GameManager.instance.UpdateScore();
+            rb.AddForce(Vector2.left * 22);
+        }
+
+        else if (collision.gameObject.tag == "CheckCollide") {
+            rb.AddForce(Vector2.down * 20);
+        }
+    }
+}
---
+++ BallBounce/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+
+    Rigidbody2D rigidB;
+    public float moveSpeed;
+    public float rightScreenEdge;
+    public float leftScreenEdge;
+
+    private void Awake() {
+        rigidB = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TouchMove();
+
+        
+    }
+
+    private void FixedUpdate() {
+        
+    }
+
+    void TouchMove() {
+        if(Input.GetMouseButton(0)) {
+           
+           
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+           float horizontal = Input.GetAxis("Horizontal");            
+
+            if (touchPos.x < 0) {
+            transform.Translate(Vector2.right * horizontal * Time.deltaTime * moveSpeed);
+                if(transform.position.x < leftScreenEdge) {
+                    transform.position = new Vector2(leftScreenEdge, transform.position.y);
+                }
+            }
+            else {
+                if(transform.position.x > rightScreenEdge) {
+                    transform.position = new Vector2(rightScreenEdge, transform.position.y);
+                }
+            }
+
+                
+
+
+
+
+            /*if(touchPos.x < 0) {
+                //move the paddle to the left
+                rigidB.velocity = Vector2.left * moveSpeed;
+            }
+            else {
+                //move the paddle to the right
+                rigidB.velocity = Vector2.right * moveSpeed;
+            }
+        }
+        else {
+            rigidB.velocity = Vector2.zero;
+        }*/
+        }
+    }
+}
